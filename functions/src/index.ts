315f038901fbe1135dfa09c8dafa9865b9ca980b
Tomoya_Suzuki <mamemochi3ko@gmail.com>
2020-08-14 16:52:10 +0900

    firestoreへの接続試し

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,24 @@
 import * as functions from 'firebase-functions';
+import * as admin from "firebase-admin";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const topPage = functions.https.onRequest((request, response) => {
-   functions.logger.info("Hello logs!", {structuredData: true});
-   response.send("HHello from Firebase!");
+export const topPage = functions.https.onRequest(async (request, response) => {
+    admin.initializeApp({
+        credential: admin.credential.applicationDefault(),
+    });
+
+    const db = admin.firestore();
+
+    const docRef = db.collection('routes').doc('testDocumentPath');
+
+    await docRef.set({
+        first: 'Ada',
+        last: 'Lovelace',
+        born: 1815
+    });
+
+    functions.logger.info("Hello logs!", {structuredData: true});
+    response.send("HHello from Firebase!");
 });
