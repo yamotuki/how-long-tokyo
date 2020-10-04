6ea4dee445b1a6d9cbe1696d21a6613d8c919192
Tomoya_Suzuki <mamemochi3ko@gmail.com>
2020-10-04 17:23:06 +0900

    nuxt経由でapi叩いて画面表示まで

--- public/nuxt/pages/index.vue
+++ public/nuxt/pages/index.vue
@@ -1,3 +1,30 @@
 <template>
-  <h1>Hello world!!!!!!!!!</h1>
+    <div>
+        <h1>駅一覧 aaaaaaaaaaaa</h1>
+        <!--    <div>
+                <div v-for="stationName in this.posts.keys()">
+                    {{ stationName }}
+                </div>
+            </div> -->
+        <div>
+            {{ this.posts }}
+        </div>
+    </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        posts: [],
+      };
+    },
+    async fetch() {
+      this.posts = await fetch(
+          'https://asia-northeast1-how-long-tokyo.cloudfunctions.net/showReachableTrigger?start=%E7%A5%9E%E6%A5%BD%E5%9D%82').
+          then(res =>
+              res.json(),
+          );
+    },
+  };
+</script>
