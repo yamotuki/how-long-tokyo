08f952e7cafc38ef7deca708ff80244bf09ac0be
Tomoya_Suzuki <mamemochi3ko@gmail.com>
2020-11-07 17:06:49 +0900

    fetch data を関数切り出し

--- public/nuxt/pages/index.vue
+++ public/nuxt/pages/index.vue
@@ -2,15 +2,14 @@
     <div>
         <h1>駅一覧</h1>
         <div style="position: relative">
+            <!--google map のスクショは、帰属をはっきりさせる部分を残せば使って良さそう。-->
             <img src="https://firebasestorage.googleapis.com/v0/b/how-long-tokyo.appspot.com/o/google_map_ss_01.jpg?alt=media&token=2ce22939-01ca-413c-b824-d64609ec0b4f"
                  alt="地図">
             <template v-for="stationName in Object.keys(station)">
                 <div :style="getStyle(station[stationName].coord.lat, station[stationName].coord.lon)">
-                    <div class="pointer-pin">
-                        p
-                    </div>
                     <div class="pointer-label">
-                        {{ stationName }}
+                        <!-- TODO: マウスオーバーした時だけ駅名が見れるようにする -->
+                        <!--{{ stationName }}-->
                         {{ station[stationName].time }}
                     </div>
                 </div>
@@ -36,25 +35,23 @@
           left: (lon - 139.552459) / (139.964959 - 139.552459) * 100 + '%',
         };
       },
+      setStart: async function($stationName) {
+        await this.fetchData($stationName);
+      },
+      fetchData: async function($stationName = '東京') {
+        const jsonRes = await fetch(
+            'http://localhost:5001/how-long-tokyo/asia-northeast1/showReachableTrigger?start=' + encodeURIComponent($stationName)).
+            then(res =>
+                res.json(),
+            );
+
+        // 直接 json parse できなかったので一度 string にしている
+        const tempString = JSON.stringify(jsonRes);
+        this.station = JSON.parse(tempString);
+      }
     },
     async fetch() {
-      const jsonRes = await fetch(
-          'http://localhost:5001/how-long-tokyo/asia-northeast1/showReachableTrigger?start=%E7%A5%9E%E6%A5%BD%E5%9D%82').
-          then(res =>
-              res.json(),
-          );
-
-      // 直接 json parse できなかったので一度 string にしている
-      const tempString = JSON.stringify(jsonRes);
-      this.station = JSON.parse(tempString);
-
-      // map をどこ基準にするか？　https://www.gsi.go.jp/KOKUJYOHO/CENTER/kendata/tokyo_heso.pdf
-      // 経度 138 ~ 140 でとりあえず表示
-      // 緯度 35 ~ 36 のなかで表示
-      // TODO sin にすれば百分率計算いい感じにできる　https://developers.google.com/maps/documentation/javascript/examples/map-coordinates
-
-      // google map のスクショは、帰属をはっきりさせる部分を残せば使って良さそう。
-
+      await this.fetchData()
     },
   };
 </script>
